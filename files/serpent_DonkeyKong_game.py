5713d68fdad0f823f6cc2bcf302bb814ee974ce8
Bastien Vanderplaetse <bast.vdp@hotmail.com>
2018-05-02 12:13:53 +0200

    Config file to launch the game

--- files/serpent_DonkeyKong_game.py
+++ files/serpent_DonkeyKong_game.py
@@ -4,7 +4,7 @@ from .api.api import DonkeyKongAPI
 
 from serpent.utilities import Singleton
 
-
+from xml.dom import minidom
 
 
 class SerpentDonkeyKongGame(Game, metaclass=Singleton):
@@ -12,18 +12,48 @@ class SerpentDonkeyKongGame(Game, metaclass=Singleton):
     def __init__(self, **kwargs):
         kwargs["platform"] = "executable"
 
-        kwargs["window_name"] = "WINDOW_NAME"
+        try:
+            doc = minidom.parse('donkeykong_config.xml')
 
-        
-        
-        kwargs["executable_path"] = "EXECUTABLE_PATH"
-        
-        
+            kwargs["window_name"] = doc.getElementsByTagName("title")[0].firstChild.nodeValue
 
-        super().__init__(**kwargs)
+            kwargs["executable_path"] = doc.getElementsByTagName("emulator")[0].firstChild.nodeValue + " " + doc.getElementsByTagName("game")[0].firstChild.nodeValue
 
-        self.api_class = DonkeyKongAPI
-        self.api_instance = None
+            super().__init__(**kwargs)
+
+            self.api_class = DonkeyKongAPI
+            self.api_instance = None
+
+        except FileNotFoundError:
+            self._create_config_file()
+
+    def _create_config_file(self):
+        print("No config file found.\nCreation of the default config file.")
+        doc = minidom.Document()
+
+        config = doc.createElement('config')
+        
+        title = doc.createElement('title')
+        title.appendChild(doc.createTextNode('WINDOW_NAME'))
+        config.appendChild(title)
+
+        path = doc.createElement('path')
+
+        emulator = doc.createElement('emulator')
+        emulator.appendChild(doc.createTextNode('EMULATOR_PATH'))
+        path.appendChild(emulator)
+
+        game = doc.createElement('game')
+        game.appendChild(doc.createTextNode('GAME_PATH'))
+        path.appendChild(game)
+
+        config.appendChild(path)
+
+        doc.appendChild(config)
+
+        doc.writexml(open('donkeykong_config.xml', 'w'), indent="    ", addindent="  ", newl='\n')
+
+        print("IMPORTANT : donkeykong_config.xml file created. Please change the default values to use the API.")
 
     @property
     def screen_regions(self):
